d290547bd14c365cc5429528c3107a29948312d9
iamDeepakA <deepakagarwal@gwu.edu>
2019-10-12 12:16:48 -0400

    added comments and detect cycle code

--- Linked_Lists/detect_cycle.py
+++ Linked_Lists/detect_cycle.py
@@ -35,4 +35,45 @@ class Linked_List:
     def print_list(self):
         nodeList = []
 
-        while
+        current = self.head
+
+        while current is not None:
+            nodeList.append(current.data)
+            current = current.next
+
+        print(nodeList)
+
+    def detectCycle(self):
+        fast_p = self.head
+        slow_p = self.head
+
+        while fast_p and slow_p and fast_p.next:
+            slow_p = slow_p.next
+            fast_p = fast_p.next.next
+
+            if slow_p == fast_p:
+                return True
+
+
+# specify the nodes
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+
+# create linked list object
+ll = Linked_List()
+
+# add nodes to linked list
+ll.addNode(node1)
+ll.addNode(node2)
+ll.addNode(node3)
+ll.addNode(node4)
+ll.addNode(node5)
+
+# create a loop for detecting cycle
+ll.head.next.next.next.next.next = ll.head
+
+# will print True if there is cycle in linked list
+print(ll.detectCycle())
