a76cc4dcde99e179128f2033b396dd4f64f45e1f
iamDeepakA <deepakagarwal@gwu.edu>
2019-10-13 18:19:53 -0400

    created code to find middle node of a linked list

---
+++ Linked_Lists/findMiddleNode.py
@@ -0,0 +1,40 @@
+from Singly_Linked_List import Node, LinkedList
+
+
+# create a new class which will inherit the methods of the base class of LinkedList
+class Linked_List(LinkedList):
+
+    def findMiddle(self):
+        fast_p = self.head
+        slow_p = self.head
+
+        while fast_p is not None:
+            fast_p = fast_p.next
+
+            if fast_p is None:
+                return slow_p
+
+            fast_p = fast_p.next
+            slow_p = slow_p.next
+
+        return slow_p
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+
+# create linked list object
+ll = Linked_List()
+
+# add nodes to linked list
+ll.addNode(node1)
+ll.addNode(node2)
+ll.addNode(node3)
+ll.addNode(node4)
+ll.addNode(node5)
+
+# reverse the list in place
+print(ll.findMiddle().get_data())
