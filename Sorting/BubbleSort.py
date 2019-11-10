29a15e9d3b1d9342a94770185a36824d10eff689
iamDeepakA <deepakagarwal@gwu.edu>
2019-11-10 18:07:30 -0500

    added code for bubble sort

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,17 +1,46 @@
-
-# coding: utf-8
-
-# In[ ]:
-
+# brute force
 
 def bubble_sort(arr):
-    # For every element (arranged backwards)
-    for n in range(len(arr)-1,0,-1):
-        #
-        for k in range(n):
-            # If we come to a point to switch
-            if arr[k]>arr[k+1]:
-                temp = arr[k]
-                arr[k] = arr[k+1]
-                arr[k+1] = temp
+    # traverse the array for all elements
+    for i in range(len(arr)):
+        # check if the last element is smaller than the previous one
+        # if yes swap
+        for j in range(len(arr) - 1, i, -1):
+            if arr[j] < arr[j - 1]:
+                swap(arr, j, j - 1)
 
+
+def swap(arr, x, y):
+    temp = arr[x]
+    arr[x] = arr[y]
+    arr[y] = temp
+
+
+arr = [2, 1, 5, 8, 7]
+
+bubble_sort(arr)
+
+print(arr)
+
+
+# improved version
+
+def improved_bubble_sort(arr):
+    for i in range(len(arr)):
+        # create a flag to keep track if inner loop swaps any element
+        swapped = 0
+
+        for j in range(len(arr) - 1, i, -1):
+            if arr[j] < arr[j - 1]:
+                swap(arr, j, j - 1)
+                swapped = 1
+        # if no element is swapped by inner loop array is already sorted - no need to run outer loop break
+        if swapped == 0:
+            break
+
+
+arr = [2, 1, 5, 8, 7]
+
+improved_bubble_sort(arr)
+
+print(arr)
