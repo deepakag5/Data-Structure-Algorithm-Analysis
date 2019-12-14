486e5c67b52961fae4d431896d4b982bcc4c6178
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-13 21:40:53 -0500

    add sum of left sub-tree

---
+++ leetcode/sumofLeftNodes.py
@@ -0,0 +1,20 @@
+def sumofleftnodesBinaryTree(root):
+    if root is None:
+        return False
+
+    # or we can directly write stack = [root]
+    stack = []
+    stack.append(root)
+
+    lsum = 0
+
+    while stack:
+        node = stack.pop()
+
+        if node.left is not None:
+            lsum += node.val
+
+        if node.right is not None:
+            stack.append(node.right)
+
+    return lsum
