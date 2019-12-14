7be359cf0cad3a1fdae0ef4635a55378d4d5fed1
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-14 14:44:23 -0500

    add find LCA iterative

--- leetcode/findLCABinarySearchTree.py
+++ leetcode/findLCABinarySearchTree.py
@@ -9,3 +9,18 @@ def findLCARecursive(root, n1, n2):
         return findLCARecursive(root.right, n1, n2)
 
     return root
+
+
+def findLCAIterative(root, n1, n2):
+    if root is None:
+        return None
+
+    while root:
+        if root.val > n1 and root.val > n2:
+            root = root.left
+        elif root.val < n1 and root.val < n2:
+            root = root.right
+        else:
+            break
+
+    return root
