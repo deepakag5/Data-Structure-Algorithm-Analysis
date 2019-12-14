2305c3925231930da7935c8579b208ee59739e8b
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-14 13:56:19 -0500

    added sorted array to BST iterative

--- leetcode/ConvertSortedArrayToBST.py
+++ leetcode/ConvertSortedArrayToBST.py
@@ -8,9 +8,31 @@ def sortedArrayBSTRecursive(arr):
 
     mid = len(arr) // 2
 
-    node = Node(arr[mid])
+    root = Node(arr[mid])
 
-    node.left = sortedArrayBSTRecursive(arr[:mid])
-    node.right = sortedArrayBSTRecursive(arr[mid + 1:])
+    root.left = sortedArrayBSTRecursive(arr[:mid])
+    root.right = sortedArrayBSTRecursive(arr[mid + 1:])
 
-    return node
+    return root
+
+
+def sortedArrayBSTIterative(arr):
+    left = 0
+    right = len(arr)
+    mid = left + right // 2
+
+    root = Node(arr[mid])
+    stack = [(root, left, mid, right)]
+
+    while stack:
+        node, left, mid, right = stack.pop()
+
+        if left != mid:
+            node.left = Node(arr[(left + mid) // 2])
+            stack.append((node.left, left, (left + mid) // 2, mid))
+
+        if right != mid + 1:
+            node.right = Node(arr[(mid + right + 1) // 2])
+            stack.append((node.right, mid, (mid + right + 1) // 2, right))
+
+    return root
