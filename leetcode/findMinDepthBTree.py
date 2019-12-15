1a816090398d00f12e8bdff99cee4b3db7d68f0d
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-15 17:29:45 -0500

    added find min depth iterative

--- leetcode/findMinDepthBTree.py
+++ leetcode/findMinDepthBTree.py
@@ -19,3 +19,29 @@ def mindepthBTreeRecursive(root):
         return mindepthBTreeRecursive(root.right) + 1
 
     return min(mindepthBTreeRecursive(root.left), mindepthBTreeRecursive(root.right)) + 1
+
+
+def mindepthBTreeIterative(root):
+    """
+
+    :param root: TreeNode
+    :return: int
+
+    """
+    if not root:
+        return 0
+
+    depth = float('inf')
+    stack = [(root, 1)]
+
+    while stack:
+        node, level = stack.pop()
+
+        if node and not node.left and not node.right:
+            depth = min(depth, level)
+
+        if node:
+            stack.append((node.left, level + 1))
+            stack.append((node.right, level + 1))
+
+    return depth
