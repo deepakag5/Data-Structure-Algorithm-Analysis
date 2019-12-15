eb604c907ce288863d5a60bc0820918411f0b565
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-15 16:22:05 -0500

    added find path sum iterative

--- leetcode/findPathSumExistsBTree.py
+++ leetcode/findPathSumExistsBTree.py
@@ -21,3 +21,31 @@ def findPathSumExistsRecursive(root, sum):
     findPathSum(root, sum, exist)
 
     return any(exist)
+
+
+def findPathExistsIterative(root, sum):
+    """
+
+    :param root: TreeNode
+    :param sum: path sum value [int]
+    :return: boolean
+
+    """
+
+    if root is None:
+        return False
+
+    stack = [(root, root.val)]
+
+    while stack:
+        node, val = stack.pop()
+
+        if node.left is None and node.right is None:
+            if val == sum:
+                return True
+        if node.right is not None:
+            stack.append((node.right, val + node.right.val))
+        if node.left is not None:
+            stack.append((node.left, val + node.left.val))
+
+    return False
