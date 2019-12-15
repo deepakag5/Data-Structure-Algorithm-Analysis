0d56308fa627487fc4217412decf29d3b03cdfd9
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-15 14:53:50 -0500

    added check balanced binary tree optimized

--- leetcode/checkifbalancedbinaryTree.py
+++ leetcode/checkifbalancedbinaryTree.py
@@ -20,3 +20,30 @@ def isBalanced(root):
         return max(left + right) + 1
 
     return check(root) != -1
+
+
+def isBalancedOptimized(root):
+    """
+
+    :param root: TreeNode
+    :return: boolean
+
+    """
+
+    def check(root):
+        if root is None:
+            return 0
+
+        left = check(root.left)
+        if left == -1:
+            return -1
+
+        right = check(root.right)
+        if right == -1:
+            return -1
+
+        if abs(left - right) > 1:
+            return -1
+        return max(left, right) + 1
+
+    return check(root) != -1
