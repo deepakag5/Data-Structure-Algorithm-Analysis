a7c5444bdcd00141d4d2e152d595e5d3d060b9cb
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-16 23:05:03 -0500

    corrected code for delete node in BST

--- leetcode/deleteBST.py
+++ leetcode/deleteBST.py
@@ -10,28 +10,28 @@ def deleteRecursive(root, node):
     if root is None:
         return root
 
-    if root.key > node.val:
-        deleteRecursive(root.left, node)
+    if root.val > node.val:
+        root.left = deleteRecursive(root.left, node)
 
-    elif root.key < node.val:
-        deleteRecursive(root.right, node)
+    elif root.val < node.val:
+        root.right = deleteRecursive(root.right, node)
 
     else:
-
-        # if the node is with only one child
+        # if the node is with only one child or no child
         if root.left is None:
             temp = root.right
             root = None
             return temp
 
-        if root.right is None:
+        elif root.right is None:
             temp = root.left
             root = None
             return temp
 
-        # if the node has two child then find the inorder successor
+        # if the node has two child then find the inorder successor (smallest in right sub-tree)
         temp = minValue(root.right)
 
+        # copy inorder successor to current node
         root.val = temp.val
 
         # delete the inorder successor
@@ -41,9 +41,16 @@ def deleteRecursive(root, node):
 
 
 def minValue(root):
+    """
+
+    :param root: TreeNode
+    :return: TreeNode
+
+    """
     node = root
 
     while node.left is not None:
         node = node.left
+    print(node.val)
 
     return node
