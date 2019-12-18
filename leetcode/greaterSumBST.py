78c4273137d86e744e4591a8a5ad29b42d2b5bfd
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-17 20:37:46 -0500

    added code for BST to GST iterative

--- leetcode/greaterSumBST.py
+++ leetcode/greaterSumBST.py
@@ -8,3 +8,26 @@ def bsttoGSTRecursive(root):
     revinorder(root, 0)
 
     return root
+
+
+# similar to inorder iterative but of course in reverse order
+def bsttoGSTIterative(root):
+    sum_nodes = 0
+    node = root
+    stack = []
+
+    while stack or node:
+        # here instead of if we used while as we want to traverse all nodes in right first
+        while node:
+            stack.append(node)
+            node = node.right
+
+        node = stack.pop()
+        # assign
+        node.val += sum_nodes
+        # accumulate
+        sum_nodes = node.val
+        # traverse
+        node = node.left
+
+    return root
