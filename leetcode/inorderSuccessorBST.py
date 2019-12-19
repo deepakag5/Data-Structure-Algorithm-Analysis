a8c87df614bea14c545abc4eba1c9e705f3165de
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-18 22:46:55 -0500

    added comments for inorder succesor for BST

--- leetcode/inorderSuccessorBST.py
+++ leetcode/inorderSuccessorBST.py
@@ -1,7 +1,19 @@
 def inorderSuccesor(root, node):
+    """
+    :param root: TreeNode
+    :param node: TreeNode
+    :return: TreeNode
+    """
+    # if right child of node is
+    # present then successor lies in right sub-tree
     if node.right is not None:
         return minValue(node.right)
 
+    # if right child is not present then successor is one of the ancestors
+    # traverse down the tree,
+    # if a node’s data is greater than root’s data
+    # then go right side,
+    # otherwise go to left side.
     while root is not None:
         if node.val < root.val:
             succ = root
@@ -15,6 +27,10 @@ def inorderSuccesor(root, node):
 
 
 def minValue(node):
+    """
+    :param node: TreeNode
+    :return: TreeNode
+    """
     current = node
 
     while current is not None:
