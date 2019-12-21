78acf9c3062fe44eea8228e5b4946c16c2d409ca
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-20 23:32:47 -0500

    added optimized sol construct B Tree from Inorder Preorder

--- leetcode/constructBTreefromInorderPreorder.py
+++ leetcode/constructBTreefromInorderPreorder.py
@@ -11,3 +11,23 @@ def constructTreeRecursive(preorder, inorder):
         root.right = constructTreeRecursive(preorder, inorder[ind + 1:])
 
         return root
+
+
+def constructTreeRecursiveOptimized(preorder, inorder):
+    inorder_map = {}
+    for i, num in enumerate(inorder):
+        inorder_map[num] = i
+
+    preorder_iter = iter(preorder)
+
+    def constructTree(start, end):
+        if start > end:
+            return None
+        root_val = next(preorder_iter)
+        root = Node(root_val)
+        ind = inorder_map[root_val]
+        root.left = constructTree(start, ind - 1)
+        root.right = constructTree(ind + 1, end)
+        return root
+
+    return constructTree(0, len(inorder) - 1)
