ffaa3fea59094c8d359b1b31ff556d8eecd018d9
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-24 22:22:41 -0500

    completed min window substring

--- leetcode/minWindowSubstring.py
+++ leetcode/minWindowSubstring.py
@@ -28,5 +28,29 @@ def minWindow(s,t):
 
     ans = float('inf'), None, None
 
+    while r<len(s):
+        char = s[r]
 
-    while r<=len(s):
+        windows_count[char] = windows_count.get(char, 0)+1
+
+        if char in dict_t and windows_count[char] == dict_t[char]:
+            freq_char_t+=1
+
+        while l<=r and freq_char_t == req_length_t:
+            char = s[l]
+
+            if r-l+1 < ans[0]:
+                ans = (r-l+1, l, r)
+
+            windows_count[char] -= 1
+
+            if char in dict_t and windows_count[char] < dict_t[char]:
+                freq_char_t-=1
+
+            l+=1
+
+        r+=1
+
+    return "" if ans[0] == float('inf') else s[ans[1]:ans[2]+1]
+
+
