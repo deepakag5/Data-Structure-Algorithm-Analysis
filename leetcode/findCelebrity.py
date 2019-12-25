5250c4e41e5a421713e07b5aef46055f59a6b8e8
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-25 12:24:06 -0500

    find celebrity added comments

--- leetcode/findCelebrity.py
+++ leetcode/findCelebrity.py
@@ -8,14 +8,17 @@ def findtheCelebrity(n):
             candidate = i
     # iterate till the position of supposed celebrity and check
     # a) if supposed celebrity knows anybody before it or
-    # b) if
+    # b) if any of the other candidates doesn't know celebrity
+    # if any of above condition if true return -1 (no celebrity)
     for i in range(candidate):
         if knows(candidate, i) or not knows(i, candidate):
             return -1
-
+    # also check if any of the candidates after supposed celebrity doesn't know him
+    # if yes then return -1 (no celebrity)
     for i in range(candidate+1, n):
         if not knows(i, candidate):
             return -1
 
-
+    # if all above conditions are met then the supposed celebrity is actually the celebrity
+    # return it
     return candidate
