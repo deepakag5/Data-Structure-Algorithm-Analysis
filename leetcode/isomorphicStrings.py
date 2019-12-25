2aba4306dfda6fff44e646e53482735771fd8580
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-25 13:06:49 -0500

    isomorphic strings

---
+++ leetcode/isomorphicStrings.py
@@ -0,0 +1,18 @@
+def isIsomorphic(s, t):
+    d1, d2 = {}, {}
+
+    for char in s:
+        if char in d1:
+            d1[char]+=1
+        else:
+            d1[char]=1
+
+    for char in t:
+        if char in d2:
+            d2[char]+=1
+        else:
+            d2[char]=1
+
+    return sorted(d1.values())==sorted(d2.values())
+
+
