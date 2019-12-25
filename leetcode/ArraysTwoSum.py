549c58d7952302d212f2b2e7c055d37ff0ab4558
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-24 19:51:42 -0500

    Optimized arrays two sum

--- leetcode/ArraysTwoSum.py
+++ leetcode/ArraysTwoSum.py
@@ -1,8 +1,22 @@
-# Brute Force
+# Brute Force  - O(n^2), O(1)
 
 def TwoSum(nums, target):
     for i in range(len(nums)):
         for j in range(1, len(nums)):
             if nums[i]+nums[j] == target:
                 return [i, j]
+
+# Optimize using dict - O(n), O(1)
+
+def TwoSumHash(nums, target):
+   nums_dict = {}
+
+   for i in range(len(nums)):
+       nums_dict[nums[i]] = i
+
+   for i in range(len(nums)):
+        complement = target - nums[i]
+
+        if complement in nums_dict and nums_dict.get(complement)!=i:
+            return [i, nums_dict.get(complement)]
 
