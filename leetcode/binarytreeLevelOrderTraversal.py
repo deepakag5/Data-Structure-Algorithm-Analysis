acbc57ab643d1151d4cb0dfd564d254ccaf3c155
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-25 13:59:31 -0500

    binary tree level order recursive

--- leetcode/binarytreeLevelOrderTraversal.py
+++ leetcode/binarytreeLevelOrderTraversal.py
@@ -16,3 +16,27 @@ def LevelOrderIterative(root):
 
         if node.right is not None:
             queue.append(node.right)
+
+
+def levelOrderRecursive(root):
+    levels = []
+
+    if not root:
+        return levels
+
+    def getlevelorder(node, level):
+        # starting the current level
+        if len(levels)==level:
+            levels.append([])
+
+        levels[level].append(node.val)
+
+        if node.left is not None:
+            getlevelorder(node.left, level+1)
+
+        if node.right is not None:
+            getlevelorder(node.right, level+1)
+
+    getlevelorder(root, 0)
+
+    return levels
