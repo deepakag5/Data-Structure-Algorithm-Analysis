13253720a7db4b118535b2803ed2bf91799b0a91
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-24 23:07:48 -0500

    completed optimized min window substring

--- leetcode/minWindowSubstring.py
+++ leetcode/minWindowSubstring.py
@@ -63,3 +63,58 @@ def minWindow(s,t):
 
 
 
+def minWindowOptimized(s, t):
+
+    if not t or not s:
+        return ""
+
+    dict_t = {}
+
+    for char in t:
+        if char in dict_t:
+            dict_t[char]+=1
+        else:
+            dict_t[char]=1
+
+    windows_count = {}
+
+    left, right = 0, 0
+
+    filtered_s = []
+
+    for i, char in enumerate(s):
+        if char in dict_t:
+            filtered_s.append((i, char))
+
+    req_len_t = len(t)
+
+    completed_t = 0
+
+    ans = float('inf'), None, None
+
+    while right<len(filtered_s):
+        char = filtered_s[right][1]
+
+        windows_count[char] = windows_count.get(char, 0) + 1
+        if windows_count[char] == dict_t[char]:
+            completed_t+=1
+
+        while left<=right and completed_t==req_len_t:
+            char = filtered_s[left][1]
+
+            end = filtered_s[right][0]
+            start = filtered_s[left][0]
+
+            if end-start+1<ans[0]:
+                ans = (end-start+1, start, end)
+
+            windows_count[char] -=1
+            if windows_count[char]<dict_t[char]:
+                completed_t-=1
+
+            left+=1
+
+        right+=1
+
+    return "" if ans[0]==float('inf') else s[ans[1]:ans[2]+1]
+
