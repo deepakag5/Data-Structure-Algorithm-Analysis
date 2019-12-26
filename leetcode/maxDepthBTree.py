1b09471c422d8758cb48212ca2b89cc578aeb648
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-25 22:55:52 -0500

    added maxdepth iterative BFS and DFS

--- leetcode/maxDepthBTree.py
+++ leetcode/maxDepthBTree.py
@@ -14,3 +14,51 @@ def maxDepthRecursive(root):
         print("right: "+str(right_height))
 
     return max(left_height, right_height) + 1
+
+
+def maxDepthIterativeDFS(root):
+    """
+    :param root: TreeNode
+    :return: int
+    """
+
+    if root is None:
+        return 0
+
+    queue = [(1, root)]
+    depth = 0
+
+    while len(queue)>0:
+        current_depth, node = queue.pop(0)
+
+        if node:
+            depth = max(depth, current_depth)
+            queue.append((current_depth+1, node.left))
+            queue.append((current_depth+1, node.right))
+
+
+    return depth
+
+
+def maxDepthIterativeBFS(root):
+    """
+    :param root: TreeNode
+    :return: int
+    """
+
+    if root is None:
+        return 0
+
+    stack = [(1, root)]
+    depth = 0
+
+    while len(stack)>0:
+        current_depth, node = stack.pop()
+
+        if node:
+            depth = max(depth, current_depth)
+            stack.append((current_depth+1, node.left))
+            stack.append((current_depth+1, node.right))
+
+
+    return depth
