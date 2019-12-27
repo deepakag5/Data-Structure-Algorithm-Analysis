482a1880e87dcedb82039b9853b1685c6a67847e
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-26 22:49:27 -0500

    removed print statements maxDepthBTree

--- leetcode/maxDepthBTree.py
+++ leetcode/maxDepthBTree.py
@@ -6,12 +6,8 @@ def maxDepthRecursive(root):
     if root is None:
         return 0
     else:
-        print("root: " + str(root.val))
         left_height = maxDepthRecursive(root.left)
         right_height = maxDepthRecursive(root.right)
-
-        print("left: "+str(left_height))
-        print("right: "+str(right_height))
 
     return max(left_height, right_height) + 1
 
