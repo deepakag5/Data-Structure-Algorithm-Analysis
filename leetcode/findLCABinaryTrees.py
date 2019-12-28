362bf4889f22a387d6ea2364853b2ee0c0977d81
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-28 14:10:34 -0500

    updated LCA B Tree

--- leetcode/findLCABinaryTrees.py
+++ leetcode/findLCABinaryTrees.py
@@ -1,3 +1,69 @@
+def lowestCommonAncestor(root, p, q):
+     if root is None:
+         return None
+
+     if root.val == p.val or root.val == q.val:
+         return root
+
+     left = lowestCommonAncestor(root.left, p, q)
+     right = lowestCommonAncestor(root.right, p, q)
+
+     if left is None:
+         return right
+
+     if right is None:
+         return left
+
+     return root
+
+
+
+# definition of binary tree
+class Node:
+    def __init__(self, key):
+        self.val = key
+        self.left = None
+        self.right = None
+
+root = Node(3)
+root.left = Node(5)
+root.right = Node(1)
+root.left.left = Node(6)
+root.left.right = Node(2)
+root.left.right.left = Node(7)
+root.left.right.right = Node(4)
+root.right.left = Node(0)
+root.right.right = Node(8)
+
+
+print(lowestCommonAncestor(root, Node(7), Node(4)).val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## another method from geeksforgeeks
+
 def find(root, v):
     """
     find the value in tree
@@ -22,11 +88,12 @@ def findLCA(root, n1, n2, v):
     :param root: TreeNode
     :param n1: val[int]
     :param n2: val[int]
+    :param v: List
     :return: TreeNode
 
     """
     if root is None:
-        return True
+        return None
 
     if root.val == n1:
         v[0] = True
@@ -36,8 +103,8 @@ def findLCA(root, n1, n2, v):
         v[1] = True
         return root
 
-    left_lca = findLCA(root.left, n1, n2)
-    right_lca = findLCA(root.right, n1, n2)
+    left_lca = findLCA(root.left, n1, n2, v)
+    right_lca = findLCA(root.right, n1, n2, v)
 
     # one key is present in left sub-tree and other in right sub-tree then lca is root
     if left_lca and right_lca:
@@ -62,8 +129,13 @@ def findLCAinBTree(root, n1, n2):
 
     lca = findLCA(root, n1, n2, v)
 
+    print("LCA: "+str(lca.val))
+
     # returns lca if both n1 and n2 are present in the tree
     if (v[0] and v[1]) or (v[0] and find(lca, n2)) or (v[1] and find(lca, n1)):
         return lca
 
     return None
+
+
+print(findLCAinBTree(root, 7, 4).val)
