245cac3112f61bf491feecb34b7e43d9a6c4b2e2
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-27 22:42:31 -0500

    completed LCA BST

--- leetcode/findLCABinarySearchTree.py
+++ leetcode/findLCABinarySearchTree.py
@@ -1,41 +1,44 @@
+# O(n), O(n)
+
 def findLCARecursive(root, n1, n2):
     """
         :param root: TreeNode
-        :param n1: val[int]
-        :param n2: val[int]
+        :param n1: TreeNode
+        :param n2: TreeNode
         :return: TreeNode
     """
 
     if root is None:
         return None
 
-    if root.val > n1 and root.val > n2:
+    if root.val > n1.val and root.val > n2.val:
         return findLCARecursive(root.left, n1, n2)
 
-    if root.val < n1 and root.val < n2:
+    elif root.val < n1.val and root.val < n2.val:
         return findLCARecursive(root.right, n1, n2)
 
-    return root
+    else:
+        return root
 
+
+# O(n), O(1)
 
 def findLCAIterative(root, n1, n2):
     """
         :param root: TreeNode
-        :param n1: val[int]
-        :param n2: val[int]
+        :param n1: TreeNode
+        :param n2: TreeNode
         :return: TreeNode
     """
+    node = root
 
-
-    if root is None:
+    if node is None:
         return None
 
-    while root:
-        if root.val > n1 and root.val > n2:
-            root = root.left
-        elif root.val < n1 and root.val < n2:
-            root = root.right
+    while node:
+        if node.val > n1.val and node.val > n2.val:
+            node = node.left
+        elif node.val < n1.val and node.val < n2.val:
+            node = node.right
         else:
-            break
-
-    return root
+            return node
