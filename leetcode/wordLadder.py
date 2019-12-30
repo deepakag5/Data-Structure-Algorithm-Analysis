eb6eecd7515f6119fb25e334dcdb8e38fe667ba5
iamDeepakA <deepakagarwal@gwu.edu>
2019-12-29 21:59:08 -0500

    added word ladder optimized

--- leetcode/wordLadder.py
+++ leetcode/wordLadder.py
@@ -54,26 +54,6 @@ def findwordladder(beginWord, endWord, wordList):
 # instead of matching with word by replacing all of its character (one at a time)
 # we match by a pattern as for Dot - *ot, D*t, Do*
 
-# Do the pre-processing on the given wordList and find all the possible generic/intermediate states.
-#  Save these intermediate states in a dictionary with key as the intermediate word and value as the list of words which have the same intermediate word.
-
-# Push a tuple containing the beginWord and 1 in a queue. The 1 represents the level number of a node.
-# We have to return the level of the endNode as that would represent the shortest sequence/distance from the beginWord.
-
-# To prevent cycles, use a visited dictionary.
-
-# While the queue has elements, get the front element of the queue. Let's call this word as current_word.
-
-# Find all the generic transformations of the current_word and find out if any of these transformations is also a transformation of other words in the word list.
-# This is achieved by checking the all_combo_dict.
-
-# The list of words we get from all_combo_dict are all the words which have a common intermediate state with the current_word.
-# These new set of words will be the adjacent nodes/words to current_word and hence added to the queue.
-
-# Hence, for each word in this list of intermediate words, append (word, level + 1) into the queue where level is the level for the current_word.
-
-# Eventually if you reach the desired word, its level would represent the shortest transformation sequence length.
-
 
 from collections import defaultdict
 
