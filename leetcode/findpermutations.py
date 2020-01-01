12503c12ce580ffdd017f393a46494eeafe4a8a1
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-01 13:17:41 -0500

    added comments in permutations file

--- leetcode/findpermutations.py
+++ leetcode/findpermutations.py
@@ -1,14 +1,23 @@
 def permute(nums):
+    """
+    :param nums: List
+    :return: List[List]
+    """
+    # create a list of list to store results
     permutations = [[]]
 
+    # iterate over all numbers
     for num in nums:
+        # list to hold the temporary results
         new_perms = []
-        for perm in permutations:
-            for i in range(len(perm)+1):
-                new_perms.append(perm[:i] + [num] + perm[i:])
+
+        # iterate over permutations
+        for perms in permutations:
+            # iterate over numbers in single permutation
+            for i in range(len(perms)+1):
+                new_perms.append(perms[:i]+[num]+perms[i:])
+        # reassign temp results to permutations list
         permutations = new_perms
 
     return permutations
-
-print(permute([1,2,3]))
 
