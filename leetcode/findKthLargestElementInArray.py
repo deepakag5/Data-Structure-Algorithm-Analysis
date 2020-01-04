c45cfcfc706f983d774b0c63c8b47fab4ba4d212
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-04 14:48:06 -0500

    added comments in find Kth largest element in array

--- leetcode/findKthLargestElementInArray.py
+++ leetcode/findKthLargestElementInArray.py
@@ -2,31 +2,46 @@ import random
 
 
 def findKthLargest(nums, k):
+    """
+    :param nums: List
+    :param k: int
+    :return: int
+    """
     def partition(left, right, pivot_index):
-
+        # get the pivot element
         pivot = nums[pivot_index]
-
+        # swap the pivot with rightmost element
         nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
 
+        # get the starting index to iterate
+        # and reach the actual position when sorted as per pivot
         j = left
 
+        # iterate over the array to shift smaller elements to the left
         for i in range(left, right):
             if nums[i] < pivot:
                 nums[j], nums[i] = nums[i], nums[j]
                 j += 1
 
+        # move the pivot to its final place
         nums[j], nums[right] = nums[right], nums[j]
 
         return j
 
     def select(left, right, k_smallest):
+        # if left and right both are 0 means there's only one element
         if left == right:
             return nums[left]
 
+        # get a random index for pivot
         pivot_index = random.randint(left, right)
 
+        # get pivot index as it would be in a sorted array by using parition
         pivot_index_sorted = partition(left, right, pivot_index)
 
+        # if index we are looking for (k_smallest) matches pivot index matches return it
+        # if k_smallest is less than pivot index then our item lies in left sub-array keep doing recursion
+        # otherwise item lies in right sub-array keep doing recursion to right of pivot till end
         if k_smallest == pivot_index_sorted:
             return nums[k_smallest]
         elif k_smallest < pivot_index_sorted:
@@ -35,6 +50,3 @@ def findKthLargest(nums, k):
             return select(pivot_index_sorted - 1, right, k_smallest)
 
     return select(0, len(nums) - 1, len(nums) - k)
-
-
-print(findKthLargest([3, 2, 1, 5, 6, 4], 2))
