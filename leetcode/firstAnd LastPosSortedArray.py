371d2c8de91975cb914100494acef178a9f1c3ad
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-04 19:12:47 -0500

    added O (log n) solution in first and last position in sorted array

--- leetcode/firstAnd LastPosSortedArray.py
+++ leetcode/firstAnd LastPosSortedArray.py
@@ -17,3 +17,26 @@ def searchRange(nums, target):
             break
 
     return [left_index, right_index]
+
+
+# O(log n) solution
+
+def searchRangeOptimized(nums, target):
+    if not nums:
+        return [-1, -1]
+
+    def search(n):
+        low, high = 0, len(nums)
+
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] >= n:
+                high = mid
+            else:
+                low = mid + 1
+
+        return low
+
+    low = search(target)
+
+    return [low, search(target + 1) - 1] if target in nums[low:low + 1] else [-1, -1]
