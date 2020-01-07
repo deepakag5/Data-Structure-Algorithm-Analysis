8adc6f8c161dbe09cfc7ff066306ac6625372c79
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-06 22:58:25 -0500

    added max product sub array shorter version

--- leetcode/maximumProductSubArray.py
+++ leetcode/maximumProductSubArray.py
@@ -11,3 +11,14 @@ def maxProductSubArray(nums):
         best = max(best, maxprod)
 
     return best
+
+
+def maxProductSubArray1(nums):
+    maxprod, minprod, best = 1, 1, nums[0]
+
+    for num in nums:
+        maxprod, minprod = max(maxprod * num, minprod * num, num), min(minprod * num, maxprod * num, num)
+
+        best = max(maxprod, best)
+
+    return best
