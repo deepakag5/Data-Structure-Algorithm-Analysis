57c00e50d29754f42d0fc20466e3f8fa26b92a1a
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-06 21:18:49 -0500

    added max sub array dynamic prog sol

--- leetcode/maximumSubArray.py
+++ leetcode/maximumSubArray.py
@@ -12,3 +12,23 @@ def maxSubArrayGreedy(nums):
         max_sum = max(max_sum, curr_sum)
 
     return max_sum
+
+
+def maxSubArrayDP(nums):
+    if not nums:
+        return 0
+
+    if len(nums) == 1:
+        return nums
+
+    max_sum = nums[0]
+
+    for i in range(len(nums)):
+        if nums[i - 1] > 0:
+            nums[i] += nums[i - 1]
+        max_sum = max(max_sum, nums[i])
+
+    return max_sum
+
+
+print(maxSubArrayDP([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
