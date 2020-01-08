af4947fed104f459601404ed6adbfe3c1c352011
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-07 21:07:55 -0500

    updated paint house I

--- leetcode/PaintHouse_I.py
+++ leetcode/PaintHouse_I.py
@@ -7,4 +7,4 @@ def paintHouse(costs):
         costs[i][1] += min(costs[i - 1][0], costs[i - 1][2])
         costs[i][2] += min(costs[i - 1][0], costs[i - 1][1])
 
-    return min(min(costs[-1][0], costs[-1][1]), costs[-1][2])
+    return min(costs[-1])
