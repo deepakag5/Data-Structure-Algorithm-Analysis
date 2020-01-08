6018ff55140080496d962edde15d12b8f0d5c423
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-07 20:37:51 -0500

    added paint house I

---
+++ leetcode/PaintHouse_I.py
@@ -0,0 +1,10 @@
+def paintHouse(costs):
+    if not costs or len(costs) == 0:
+        return 0
+
+    for i in range(1, len(costs)):
+        costs[i][0] += min(costs[i - 1][1], costs[i - 1][2])
+        costs[i][1] += min(costs[i - 1][0], costs[i - 1][2])
+        costs[i][2] += min(costs[i - 1][0], costs[i - 1][1])
+
+    return min(min(costs[-1][0], costs[-1][1]), costs[-1][2])
