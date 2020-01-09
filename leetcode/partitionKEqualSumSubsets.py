44250706cdc5f223acac3b2746b3b57563a102be
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-08 21:15:58 -0500

    added comments partition K equal sum subsets construct by subset sums sol

--- leetcode/partitionKEqualSumSubsets.py
+++ leetcode/partitionKEqualSumSubsets.py
@@ -42,36 +42,46 @@ def canPartition(start, nums, used_nums, k, inProgressSum, targetBucketSum):
 
 # search by constructing subset sums
 
-def canPartitionKSubsets_1(nums, k):
-    target = sum(nums) / k
+def canPartitionKSubsets(nums, k):
+    target = int(sum(nums) / k)
 
+    # if reminder is not zero then cannot divide in equal int buckets
     if sum(nums) % k != 0:
         return False
 
     def search(groups):
+        # base case if list is empty
         if not nums:
             return True
+        # get the last number (highest as we have sorted) in the list
         v = nums.pop()
 
+        # iterate for each bucket (value in the bucket list)
         for i, group in enumerate(groups):
+            # if value in the bucket is less than the target then add the last value
             if group + v <= target:
                 groups[i] += v
-
+                # if we have succesfully filled the buckets
                 if search(groups):
                     return True
-
                 groups[i] -= v
-            if not groups:
+            if not group:
                 break
 
         return False
 
+    # sort the list
     nums.sort()
+    # if the largest num in list is greater than target then
+    # list cannot be divided in K equal sum buckets
     if nums[-1] > target:
         return False
-
+    # if any of the num matches the target then it
+    # should be alone in the bucket (assuming no zeroes)
+    # remove the number from the list and also the number of buckets
     while nums and nums[-1] == target:
         nums.pop()
         k -= 1
 
+    # we have passed a list of K-1 buckets with zero value
     return search([0] * k)
