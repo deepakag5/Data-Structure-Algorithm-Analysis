d9d36b7d4e245a656f79e8734c8b2e226c03a769
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-10 23:20:06 -0500

    added flatten nested list optimized O(h) sol

--- leetcode/flattenNestedListIterator.py
+++ leetcode/flattenNestedListIterator.py
@@ -14,3 +14,30 @@ class NestedIterator:
             self.stack = self.stack[:-1] + top.getList()[::-1]
 
         return False
+
+
+class NestedIterator:
+    def __init__(self, nestedList):
+        self.stack = [[nestedList, 0]]
+
+    def next(self):
+        self.hasNext()
+        nestedList, i = self.stack[-1]
+        self.stack[-1][1] += 1
+        return nestedList[i].getInteger()
+
+    def hasNext(self):
+        s = self.stack
+        while s:
+            nestedList, i = self.stack[-1]
+            if i == len(nestedList):
+                s.pop()
+            else:
+                x = nestedList[i]
+                if x.isInteger():
+                    return True
+
+                s[-1][1] += 1
+                s.append([x.getList(), 0])
+
+        return False
