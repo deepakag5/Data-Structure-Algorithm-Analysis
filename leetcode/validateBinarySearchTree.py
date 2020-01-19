62e2cf9270f5fee75c0ec79edee301b3a7625ddb
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-19 10:44:35 -0500

    added comments to validateIterative in validate BST

--- leetcode/validateBinarySearchTree.py
+++ leetcode/validateBinarySearchTree.py
@@ -6,6 +6,8 @@ def validateRecursive(root):
         return True
 
     def recurse(node, lower=float('-inf'), upper=float('inf')):
+        if not node:
+            return True
         val = node.val
         if val <= lower or val >= upper:
             return False
@@ -31,13 +33,19 @@ def validateIterative(root):
 
     while stack:
         node, lower, upper = stack.pop()
-
         if not node:
             continue
-        val = root.val
+        val = node.val
+        print(val)
+        print("lower: " + str(lower))
+        print("upper: " + str(upper))
 
         if val <= lower or val >= upper:
             return False
+        # we are appending right and then left node in stack so when loop
+        # gets called again last value appended which is left will get popped (LIFO)
+        # also for right node you want to compare with its left value hence change lower to val
+        # for left node you want compare with right node value hence change upper to val
         stack.append((node.right, val, upper))
         stack.append((node.left, lower, val))
 
@@ -64,3 +72,20 @@ def validateInorder(root):
         root = root.right
 
     return True
+
+
+class Node:
+
+    def __init__(self, key):
+        self.val = key
+        self.left = None
+        self.right = None
+
+
+root = Node(2)
+root.left = Node(1)
+root.right = Node(5)
+root.right.left = Node(4)
+root.right.right = Node(6)
+
+print(validateIterative(root))
