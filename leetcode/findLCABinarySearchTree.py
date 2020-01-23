e975b1b2f14caf987628d5b45177bbc9226fd22c
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-22 19:07:05 -0500

    added comments in findLCA_BST

--- leetcode/findLCABinarySearchTree.py
+++ leetcode/findLCABinarySearchTree.py
@@ -16,7 +16,6 @@ def findLCARecursive(root, n1, n2):
 
     elif root.val < n1.val and root.val < n2.val:
         return findLCARecursive(root.right, n1, n2)
-
     else:
         return root
 
@@ -32,13 +31,18 @@ def findLCAIterative(root, n1, n2):
     """
     node = root
 
+    # base case
     if node is None:
         return None
 
     while node:
+        # if both values are lesser than current node then by definition of BST both lie in left sub tree
         if node.val > n1.val and node.val > n2.val:
             node = node.left
+        # consequently if both values are greater they lie in right sub tree
         elif node.val < n1.val and node.val < n2.val:
             node = node.right
         else:
+            # we have found the split point as one value is smaller (lies in left subtree)
+            # and other is higher (lies in right subtree)
             return node
