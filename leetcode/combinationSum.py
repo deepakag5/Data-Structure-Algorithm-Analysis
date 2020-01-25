985a0db768c8bce7e28df868266c1b03bed47f99
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-25 00:31:20 -0500

    added time complexity combination sum

--- leetcode/combinationSum.py
+++ leetcode/combinationSum.py
@@ -1,3 +1,10 @@
+# k is average length of each solution, and we need O(k) time to copy new linkedlist when we get one combination.
+# For example, given int[] arr = {2, 3, 4, 5, 6} and target is 10 and each element can be used for MORE than once.
+# Actually, it is same with the problem: given int[] arr = {2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6}, and target 10,
+# each element can be used for ONLY one time, which is the same description of Combination Sum II.
+#  And you must find that for the new array, each element E, which is smaller than target, will expand to ceil(target/E).
+#  Assume the new array has length n', we can get the time complexity which is O(k * 2 ^ n')
+
 def combSum(candidates, target):
     res = []
 
