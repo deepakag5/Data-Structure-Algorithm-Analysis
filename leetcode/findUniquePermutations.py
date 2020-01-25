71c8f7f5fe2f521b1cbbe699e791d31416f70429
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-25 11:30:13 -0500

    added backtracking sol find unique permutations

--- leetcode/findUniquePermutations.py
+++ leetcode/findUniquePermutations.py
@@ -17,3 +17,24 @@ def permuteUniqueIterative(nums):
         permutations = new_perms
 
     return permutations
+
+
+def permsbacktrack_II(nums):
+    def backtrack(temp_list, first=0):
+        if len(temp_list) == len(nums):
+            output.append(temp_list)
+            return
+
+        used = {}
+        for i in range(first, n):
+            if nums[i] in used:
+                continue
+            used[nums[i]] = True
+            nums[first], nums[i] = nums[i], nums[first]
+            backtrack(temp_list + [nums[first]], first + 1)
+            nums[first], nums[i] = nums[i], nums[first]
+
+    n = len(nums)
+    output = []
+    backtrack([])
+    return output
