3bee632e645954a92852e0928348ecbd51491afc
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-24 20:21:15 -0500

    added sol using list to find second minimum in binary tree

--- leetcode/findSecondMinimuminBTree.py
+++ leetcode/findSecondMinimuminBTree.py
@@ -42,3 +42,30 @@ def findSecondMinOptimumSol(self, root):
     traverse(root)
 
     return self.second_min if self.second_min < float('inf') else -1
+
+
+def findSecondMinOptimumSol_1(root):
+    """
+    Optimum solution without the use of instance variable
+
+    Note : In Python 2 you cannot modify references to
+    variables within functions outside of its scope.
+    By wrapping the value in a list, it allows modification of
+    the value without modifying the reference to the list variable.
+    Hence we have
+    """
+    second_min = [float('inf')]
+    min_val = root.val
+
+    def traverse(node):
+        if not node:
+            return
+        if min_val < node.val < second_min[0]:
+            second_min[0] = node.val
+        elif node.val == min_val:
+            traverse(node.left)
+            traverse(node.right)
+
+    traverse(root)
+
+    return second_min[0] if second_min[0] < float('inf') else -1
