d6e8673962e84218fa3a3e340940433a8fa7d943
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-26 14:18:42 -0500

    added single function to return array in maxsubarray

--- leetcode/maximumSubArray.py
+++ leetcode/maximumSubArray.py
@@ -35,71 +35,24 @@ def maxSubArrayDP(nums):
     return max_sum
 
 
-def maxSubArrayGreedy_returnarray(nums):
-    if not nums:
-        return 0
+def maxSubArraySum_returnArray(nums):
+    max_sum = float('-inf')
+    curr_sum = 0
+    # start = 0
+    # end = 0
+    # s = 0
 
-    if len(nums) == 1:
-        return nums
+    for i in range(0, len(nums)):
 
-    curr_sum = max_sum = nums[0]
+        curr_sum += nums[i]
 
-    # specify variables for starting
-    # and ending index of max sub-array
-    start = [0]
-    end = [0]
-
-    for i in range(1, len(nums)):
-        curr_sum = max(nums[i], curr_sum + nums[i])
-
-        # whenever curr sum is less than 0
-        # increment start
-        if curr_sum < 0:
-            start[0] = i + 1
-
-        # whenever max sum is less than curr sum
-        # update ending index
         if max_sum < curr_sum:
-            end[0] = i
+            max_sum = curr_sum
+            # start = s
+            # end = i
 
-        max_sum = max(max_sum, curr_sum)
+        if curr_sum < 0:
+            curr_sum = 0
+            # s = i + 1
 
-    return max_sum, nums[start[0]:end[0] + 1]
-
-
-def maxSubArrayDP_returnarray(nums):
-    if not nums:
-        return 0
-
-    if len(nums) == 1:
-        return nums
-
-    # As we are modifying the array in-place
-    # we need make a copy of array to return
-    # the max sum sub array later
-    nums_copy = nums[:]
-
-    max_sum = nums[0]
-
-    # specify variables for starting
-    # and ending index of max sub-array
-    start = [0]
-    end = [0]
-
-    for i in range(1, len(nums)):
-        if nums[i - 1] > 0:
-            nums[i] += nums[i - 1]
-
-        # whenever curr sum is less than 0
-        # increment start
-        if nums[i] < 0:
-            start[0] = i + 1
-
-        # whenever max sum is less than curr sum
-        # update ending index
-        if max_sum < nums[i]:
-            end[0] = i
-
-        max_sum = max(max_sum, nums[i])
-
-    return max_sum, nums_copy[start[0]:end[0] + 1]
+    return max_sum  # , nums[start:end+1]
