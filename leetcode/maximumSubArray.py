6ca6f440673c6a8325161d9f2963ecda0330a3e8
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-26 13:16:23 -0500

    added code to return sub array in maxsubarray

--- leetcode/maximumSubArray.py
+++ leetcode/maximumSubArray.py
@@ -24,11 +24,69 @@ def maxSubArrayDP(nums):
     max_sum = nums[0]
 
     for i in range(1, len(nums)):
+        # print(nums[i-1])
         if nums[i - 1] > 0:
             nums[i] += nums[i - 1]
+
         max_sum = max(max_sum, nums[i])
 
     return max_sum
 
 
-print(maxSubArrayDP([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+def maxSubArrayGreedy_returnarray(nums):
+    if not nums:
+        return 0
+
+    if len(nums) == 1:
+        return nums
+
+    curr_sum = max_sum = nums[0]
+
+    start = [0]
+    end = [0]
+    for i in range(1, len(nums)):
+        curr_sum = max(nums[i], curr_sum + nums[i])
+
+        if curr_sum < 0:
+            start[0] = i + 1
+
+        if max_sum < curr_sum:
+            end[0] = i
+
+        max_sum = max(max_sum, curr_sum)
+
+    print("start: " + str(start[0]))
+    print("end: " + str(end[0]))
+
+    return max_sum, nums[start[0]:end[0] + 1]
+
+
+def maxSubArrayDP_returnarray(nums):
+    if not nums:
+        return 0
+
+    if len(nums) == 1:
+        return nums
+
+    # make a copy of nums to return the max sum subarray later
+    nums_copy = nums[:]
+
+    max_sum = nums[0]
+
+    start = [0]
+    end = [0]
+
+    for i in range(1, len(nums)):
+        # print(nums[i-1])
+        if nums[i - 1] > 0:
+            nums[i] += nums[i - 1]
+
+        if nums[i] < 0:
+            start[0] = i + 1
+
+        if max_sum < nums[i]:
+            end[0] = i
+
+        max_sum = max(max_sum, nums[i])
+
+    return max_sum, nums_copy[start[0]:end[0] + 1]
