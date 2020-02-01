7728bef08e2ee869f31585bc6c9469c3561781e2
iamDeepakA <deepakagarwal@gwu.edu>
2020-02-01 13:03:06 -0500

    added two sum less than K

---
+++ leetcode/twoSumLessthanK.py
@@ -0,0 +1,13 @@
+def twoSum(arr, target):
+    arr = sorted(arr)  # O (N log N)
+    i, j = 0, len(arr) - 1
+    ans = -1
+
+    while i < j:
+        if arr[i] + arr[j] < target:
+            ans = max(ans, arr[i] + arr[j])
+            i += 1
+        else:
+            j += 1
+
+    return ans
