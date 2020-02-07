2cbc54079de25b397210293e69efc65d818d3e93
iamDeepakA <deepakagarwal@gwu.edu>
2020-02-06 22:17:06 -0500

    added find most common K words

--- leetcode/array_mostCommonKWords.py
+++ leetcode/array_mostCommonKWords.py
@@ -1,6 +1,7 @@
-# O(N log N)
-from collections import Counter
+# Time: O(N log N)
+# Space: O(N)
 
+from collections import Counter
 
 def mostCommonKWords(word_list, k):
     count = Counter(word_list)
@@ -9,13 +10,12 @@ def mostCommonKWords(word_list, k):
     return candidates[:k]
 
 
-# O(N log k)
-# O(N) 
-import heapq
+# Time: O(N log k)
+# Space: O(N)
+
+import heapq
 
 
-def  mostCommonKWords_Optimized(word_list, k):
-
-
-count = Counter(word_list)
-return heapq.nsmallest(k, count.keys(), key = lambda w: (-count[w], w))
+def mostCommonKWords_Optimized(word_list, k):
+    count = Counter(word_list)
+    return heapq.nsmallest(k, count.keys(), key=lambda w: (-count(w), w))
