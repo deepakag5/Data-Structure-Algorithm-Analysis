713bc2ee3ec47b9fd19c6bc5bb4cd4f49db712c1
iamDeepakA <deepakagarwal@gwu.edu>
2020-03-15 14:00:24 -0400

    add reverse words

---
+++ leetcode/array_reverseWords.py
@@ -0,0 +1,13 @@
+def reverseWords(s):
+    def reverseString(string):
+        left, right = 0, len(string) - 1
+        while left < right:
+            string[left], string[right] = string[right], string[left]
+            left, right = left + 1, right - 1
+
+        return "".join(string)
+
+    return " ".join([reverseString(list(st)) for st in s.split()])
+
+    ## we can even do below
+#   return " ".join([st[::-1] for st in s.split()])
