f61b88aea930782bc7f7e0a4601bc9bca752c116
iamDeepakA <deepakagarwal@gwu.edu>
2020-03-31 13:18:36 -0400

    corrected - merge two sorted lists - 1

--- leetcode/mergeTwoSortedLists.py
+++ leetcode/mergeTwoSortedLists.py
@@ -10,10 +10,10 @@ def mergeRecursive(self, l1, l2):
         return l2
 
     if l1.val < l2.val:
-        l1.next = self.merge(l1.next, l2)
+        l1.next = self.mergeRecursive(l1.next, l2)
         return l1
     else:
-        l2.next = self.merge(l1, l2.next)
+        l2.next = self.mergeRecursive(l1, l2.next)
         return l2
 
 
