abf4534d35a8e319cf2a873bfcedcc8a83bed984
iamDeepakA <deepakagarwal@gwu.edu>
2020-05-30 09:48:23 -0400

    modified array add strings and added comments

--- leetcode/array_AddStrings.py
+++ leetcode/array_AddStrings.py
@@ -1,7 +1,9 @@
 # Time: O(max(N,M))
 # Space: O(max(N,M))
 
+
 def addStrings(num1, num2):
+    # edge cases
     if len(num1) == 0:
         return num2
     if len(num2) == 0:
@@ -15,17 +17,26 @@ def addStrings(num1, num2):
         sum_val = carry
 
         if i >= 0:
-            sum_val += ord(num1[i]) - '0'
+            # ord will give the ascii value of char-int
+            # we are getting difference of each one from 0
+            # so that we get actual integer num
+            # for example ascii value of 5 is 53 and that of 0 is 48
+            # so 53-48 = 5 !!
+            sum_val += ord(num1[i]) - ord('0')
             i -= 1
         if j >= 0:
-            sum_val += ord(num2[i]) - '0'
+            sum_val += ord(num2[i]) - ord('0')
             j -= 1
 
+        # append the remainder (for example 12%10 we want to append 2)
         result.append(sum_val % 10)
 
+        # get the quotient
         carry = sum_val // 10
 
+    # if still carry has any value (which would have come from prev additions)
     if carry != 0:
         result.append(carry)
 
+    # as we have started to add from the end we need to reverse and return as str
     return "".join(str(i) for i in result[::-1])
