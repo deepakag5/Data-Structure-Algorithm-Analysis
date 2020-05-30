bb2eebe9bf2cc8466e2c4ffd3b33914bdc564dc3
iamDeepakA <deepakagarwal@gwu.edu>
2020-05-30 15:36:29 -0400

    added continuous subarray sum K - optimized

--- leetcode/array_continuousSubArraySumK.py
+++ leetcode/array_continuousSubArraySumK.py
@@ -25,3 +25,43 @@ def conSubArraySumK(nums, k):
             return True
 
     return False
+
+
+# Optimized using hashmap
+# Time: O(n)
+# Space: O(min(n,k))
+
+def conSubArraySumK_hm(nums, k):
+    if len(nums) == 0:
+        return False
+
+    if len(nums) == 1 and nums[0] % k != 0:
+        return False
+
+    # to store the remainder of sum at location - i
+    presum = {}
+    presum[0] = -1
+    sum_val = 0
+
+    for i, num in enumerate(nums):
+        sum_val += num
+
+        if k != 0:
+            sum_val = sum_val % k
+
+        # if the remainder has been seen before
+        # then both the numbers are in same group
+        # if the results of two integers i1 and i2 modulo k are the same, then we can say they are in the same group
+        # e.g. 7 % 13 = 7 and 46 % 13 = 7, so we can think of 7 and 46 are in the same group
+        # we can name the group using the result of modulo
+        # if we know two integers i1 and i2 are in the same group d, then we have:
+        # i1 = 13 * a + d and i2 = 13 * b + d where a and b are both integers
+        # if then we calculate their difference, we can get:
+        # i1 - i2 = 13 * a - 13 * b + d - d = 13 * (a - b)
+        if sum_val in presum:
+            if i - presum[sum_val] >= 2:
+                return True
+        else:
+            presum[sum_val] = i
+
+    return False
