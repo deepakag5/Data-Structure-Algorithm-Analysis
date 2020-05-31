15d90a14594bf5d2debbc40e5807617130f58c4b
iamDeepakA <deepakagarwal@gwu.edu>
2020-05-31 13:03:56 -0400

    renamed files

---
+++ leetcode/array_mergeIntervals.py
@@ -0,0 +1,25 @@
+# sorting of list and the linear scan
+# O(n log n) + O(n) = O(n log n)
+# space - O (n) for holding the results
+
+def merge(intervals):
+    # base case
+    if not intervals:
+        return []
+    # sort the list on basis of first value in each sub-list
+    intervals.sort(key=lambda x: x[0])
+    merged = []
+
+    for i in intervals:
+        # if the list of merged intervals is empty
+        # or if the current interval does not overlap with the previous,
+        # simply append it.
+        if not merged or merged[-1][-1] < i[0]:
+            merged.append(i)
+        # otherwise, there is overlap,
+        # so we merge the current and previous intervals
+        # by getting max in last value of the sub-lists
+        else:
+            merged[-1][-1] = max(merged[-1][-1], i[-1])
+
+    return merged
