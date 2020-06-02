8233a994a167028de7c5e3d90c612b532f3f4bda
iamDeepakA <deepakagarwal@gwu.edu>
2020-06-01 22:31:00 -0400

    added space optimized version for anagrams

--- leetcode/array_Anagram.py
+++ leetcode/array_Anagram.py
@@ -1,3 +1,6 @@
+# Time: O(N)
+# Space: O(2N) - used two hashmaps to store the chars
+
 def isAnagram(s, t):
     if len(s) != len(t):
         return False
@@ -10,3 +13,25 @@ def isAnagram(s, t):
         dict2[item] = dict2.get(item, 0) + 1
 
     return dict1 == dict2
+
+
+# Time: O(N)
+# Space: O(N) - only one hashmap to store the chars
+
+def isAnagramOpt(s, t):
+    if len(s) != len(t):
+        return False
+
+    dict1 = {}
+
+    for item in s:
+        dict1[item] = dict1.get(item, 0) + 1
+
+    for item in t:
+        dict1[item] = dict1.get(item, 0) - 1
+
+    for value in dict1.values():
+        if value != 0:
+            return False
+
+    return True
