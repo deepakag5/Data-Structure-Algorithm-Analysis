91ca2f0c7ad5255bf4d2ed84ab8f5151fd43c89f
iamDeepakA <deepakagarwal@gwu.edu>
2020-06-07 11:44:45 -0400

    added comment to sorted array missing element

--- leetcode/array_sorted_array_missing_element.py
+++ leetcode/array_sorted_array_missing_element.py
@@ -31,16 +31,21 @@ def missingElementBinary(nums, k):
     if len(nums) == 0:
         return 0
 
+    # Return how many numbers are missing until nums[idx]
     def missing(idx):
         return nums[idx] - nums[0] - idx
 
     n = len(nums)
 
+    # If kth missing number is larger than
+    # the last element of the array
     if k > missing(n - 1):
         return nums[n - 1] + k - missing(n - 1)
 
     left, right = 0, n - 1
 
+    # find left = right index such that
+    # missing(left - 1) < k <= missing(left)
     while left != right:
         pivot = (left + (right - left)) // 2
 
@@ -49,4 +54,6 @@ def missingElementBinary(nums, k):
         else:
             right = pivot
 
+    # kth missing number is greater than nums[left - 1]
+    # and less than nums[left]
     return nums[left - 1] + k - missing(left - 1)
