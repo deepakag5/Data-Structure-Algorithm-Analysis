2fa7a663e6d382851977996f83bb4dfcc5dfdbb5
iamDeepakA <deepakagarwal@gwu.edu>
2020-06-14 13:15:51 -0400

    added generate parenthesis -  comments

--- leetcode/puzzle_generate_parenthesis.py
+++ leetcode/puzzle_generate_parenthesis.py
@@ -9,11 +9,15 @@ def generate_parenthesis(n):
     result = []
 
     def backtrack(s='', left=0, right=0):
+        # we need to have a closing bracket for each opening bracket
+        # so a pair hence 2*n
         if len(s) == 2 * n:
             result.append(s)
             return
+        # we need open brackets at most the length of n
         if left < n:
             backtrack(s + '(', left + 1, right)
+        # we will place a closing bracket only after we have an open bracket
         if right < left:
             backtrack(s + ')', left, right + 1)
 
