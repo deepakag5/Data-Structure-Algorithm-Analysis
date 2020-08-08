24d2f6b044ece19f3e7555eb2427e069186df718
iamDeepakA <deepakagarwal@gwu.edu>
2020-08-08 15:16:58 -0400

    added code for average in Binary Tree DFS - space optimized

--- leetcode/binarytree_average_at_depth.py
+++ leetcode/binarytree_average_at_depth.py
@@ -46,3 +46,39 @@ def average_by_level(node):
         result.append(avg)
 
     return result
+
+
+# DFS space optimized version
+# Time: O(n) -- we must traverse all the elements in the tree to calculate average at each level
+# Space: O(k) -- where k is the number of levels (we are storing only the total sum and count for each level)
+
+
+def collect_opt(node, hash_map, depth=0):
+    if not node:
+        return None
+
+    if depth not in hash_map:
+        hash_map[depth] = (node.val, 1)
+    else:
+        val, count = hash_map[depth]
+        val += node.val
+        count += 1
+        hash_map[depth] = (val, count)
+
+    collect_opt(node.left, hash_map, depth + 1)
+    collect_opt(node.right, hash_map, depth + 1)
+
+
+def average_opt(node):
+    hash_map = {}
+
+    collect_opt(node, hash_map)
+
+    result = []
+
+    for level in hash_map:
+        level_sum, count = hash_map[level]
+        avg = level_sum / count
+        result.append(avg)
+
+    return result
