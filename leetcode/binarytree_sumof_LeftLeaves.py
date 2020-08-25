5174ce2fd09546e157ea4444d5e320ef5d9b1a23
iamDeepakA <deepakagarwal@gwu.edu>
2020-08-24 23:15:58 -0400

    corrected and added more recursive solutions to sum of left leaves btree

--- leetcode/binarytree_sumof_LeftLeaves.py
+++ leetcode/binarytree_sumof_LeftLeaves.py
@@ -1,21 +1,63 @@
-def sumOfLeftLeavesBinaryTree(root):
-    if root is not None:
-        return False
+# Time: O(N)
+# Space: O(N)
+
+
+def sumOfLeftLeavesRecursive(root):
+    left_leaves_sum = [0]
+
+    def dfs(root):
+        if not root:
+            return 0
+
+        if not root.left and not root.right:
+            return root.val
+
+        left_val = dfs(root.left)
+        left_leaves_sum[0] += left_val
+
+        dfs(root.right)
+
+        return 0
+
+    dfs(root)
+    return left_leaves_sum[0]
+
+
+def sumOfLeftLeavesRecursive_1(root):
+    left_leaves_sum = [0]
+
+    def dfs(root):
+        if not root:
+            return
+
+        if root.left and not root.left.left and not root.left.right:
+            left_leaves_sum[0] += root.left.val
+
+        dfs(root.left)
+        dfs(root.right)
+
+    dfs(root)
+
+    return left_leaves_sum[0]
+
+
+def sumOfLeftLeavesIterative(root):
+    if not root:
+        return 0
+
+    left_leaves_sum = 0
 
     stack = [root]
-
-    llsum = 0
 
     while stack:
         node = stack.pop()
 
-        if node.left is not None:
+        if node.left:
             stack.append(node.left)
+            if not node.left.left and not node.left.right:
+                left_leaves_sum += node.left.val
 
-            if node.left.left is None and node.left.right is None:
-                llsum += node.val
-
-        if node.right is not None:
+        if node.right:
             stack.append(node.right)
 
-    return llsum
+    return left_leaves_sum
